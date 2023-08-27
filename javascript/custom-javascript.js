4a69dfff6c85ba9337bc6e2c00d2419ec89e76a8
Matt Murmello <123772218+themurm523@users.noreply.github.com>
2023-08-27 00:08:23 -0400

    Add files via upload

---
+++ javascript/custom-javascript.js
@@ -0,0 +1,10 @@
+function darkmode() {
+            var element = document.body;
+            // var style = document.getElementById(darkmode-icon)
+            element.className = "dark-mode";
+
+        }
+        function lightmode() {
+            var element = document.body;
+            element.className = "light-mode";
+        }
